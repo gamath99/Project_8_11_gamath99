5bce2d2944ab404ec08907dd74cb14f8076cb799
gamath99 <gamamathurin1@gmail.com>
2026-07-17 16:14:51 -0400

    Create the main core for game

---
+++ game_toukay.py
@@ -0,0 +1,25 @@
+""" Project 2
+Gama Mathurin 
+The count and capture Game 
+The game consists in collecting more points (stones) then the opponents while moving the points (stones) in a clockwise way from a board 2*6. 
+The Player scores by adding a point in a 3 point cell, 
+but he can only capture the stones if the last distributed point drop in the 3 point cell. 
+
+"""
+
+class Toukay_game:
+    """Initialize the game, distribute points"""
+
+    def __init__(self):
+        pass
+        
+
+    def play(self):
+        # Implement the main game loop here
+        pass
+
+
+
+if __name__ == "__main__":
+    game = Toukay_game()
+    game.play()  # Start the game loop
